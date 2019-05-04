ce4e76c0aa78359978e3194efc6f31dab878aa2f
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-05-03 17:25:37 -0700

    bug

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -27,8 +27,7 @@ class BaseDataset(data.Dataset, ABC):
             opt (Option class)-- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
         self.opt = opt
-        self.root = opt.dataroot
-
+        
     @staticmethod
     def modify_commandline_options(parser, is_train):
         """Add new dataset-specific options, and rewrite default values for existing options.
